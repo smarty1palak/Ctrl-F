c181bd7f9069a789702d894dea2d9b721a44bc9b
smarty1palak <smarty1palak@gmail.com>
2020-04-15 23:41:42 +0530

    firebase authentication added

---
+++ ctrl_f/lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:ctrlf/authentication.dart';
+import 'package:ctrlf/root_page.dart';
+import 'package:ctrlf/login_signup_page.dart';
+import 'package:ctrlf/main.dart';
+
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Flutter login demo"),
+      ),
+      body: new Container(
+        child: new Text("Hello World"),
+      ),
+    );
+  }
+}
